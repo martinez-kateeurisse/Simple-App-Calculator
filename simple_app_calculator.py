e03aa48451f02f97fe5d4541ca68b33a19dca662
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-06 16:19:54 +0800

    Pseudocode

---
+++ simple_app_calculator.py
@@ -0,0 +1,11 @@
+#Kate Eurisse Martinez_BSCPE 1-5_Simple App Calculator
+
+#This program will create a Simple App Calculator
+
+#The application will ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division)
+#The application will ask the user for two numbers
+#Display the result
+#The application will ask if the user wants to try again or not.
+#If yes, repeat Step 1.
+#If no, Display “Thank you!” and the program will exit 
+#Use Python Function and appropriate Exceptions to capture errors during runtime.
