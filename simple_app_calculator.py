0565a2248638cb131c574c40e3330e3b2f7ef795
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-07 18:22:02 +0800

    Turning past code as comments for reference

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -14,48 +14,48 @@ import tkinter as tk
 #Start the window loops
 
 #Initialize retrying variable
-retry = "y"
+#retry = "y"
 #Loop condition
-while retry == "y":
+#while retry == "y":
     #Use Python Function and appropriate Exceptions to capture errors during runtime.
-    while True:
+    #while True:
         #Try function to test the block of codes
-        try:
+        #try:
             #Display instructions regarding the operations
-            print (" Enter + if Addition \n Enter - if Subtraction \n Enter * if Multiplication \n Enter / if Division")
+        #    print (" Enter + if Addition \n Enter - if Subtraction \n Enter * if Multiplication \n Enter / if Division")
             #Ask the user to enter their chosen operation
-            operation = input("Please enter the corresponding symbol for your chosen operator: ")
-            operators = ['+', '-', '*', '/']
-            if  operation not in operators:
-                raise Exception("Sorry, operation should only be +, -, *, or /:") 
+        #    operation = input("Please enter the corresponding symbol for your chosen operator: ")
+        #    operators = ['+', '-', '*', '/']
+        #    if  operation not in operators:
+        #        raise Exception("Sorry, operation should only be +, -, *, or /:") 
             #Ask the user for two numbers
-            num1 = float(input("Please input the first number: "))            
-            num2 = float(input("Please input the second number: "))
+        #    num1 = float(input("Please input the first number: "))            
+        #    num2 = float(input("Please input the second number: "))
             #Perform the operation chosen with the two numbers
             #If operation is addition
-            if operation == "+":
-                result = num1 + num2     
+        #    if operation == "+":
+        #        result = num1 + num2     
             #If operation is subtraction
-            elif operation == "-":
-                result = num1 - num2
+        #    elif operation == "-":
+        #        result = num1 - num2
             #If operation is multiplication
-            elif operation == "*":
-                result = num1 * num2
+        #    elif operation == "*":
+        #        result = num1 * num2
             #If operation is division
-            elif operation == "/":
-                result = num1/num2
+        #    elif operation == "/":
+        #        result = num1/num2
             #Display the result
-            print(result)
+        #    print(result)
         #Except function to handle errors
-        except ZeroDivisionError:
-            print("Sorry! You are dividing by zero. Try changing the second number.")
-        except ValueError:
-            print("Invalid input: Please input numbers only")
-        except Exception:
-            print("Sorry, input should only be +, -, *, or /d")             
-        break
+        #except ZeroDivisionError:
+        #    print("Sorry! You are dividing by zero. Try changing the second number.")
+        #except ValueError:
+        #    print("Invalid input: Please input numbers only")
+        #except Exception:
+        #    print("Sorry, input should only be +, -, *, or /d")             
+        #break
         #Ask if the users if they want to try again or not.
-    retry = input("Do you want to try again?(Enter 'y' if yes and any key if no) ")
+    #retry = input("Do you want to try again?(Enter 'y' if yes and any key if no) ")
     #If yes, the program will repeat Step 1.
 #If no, the program will display “Thank you!” and the program will exit
-print("Thank you!") 
+#print("Thank you!") 
